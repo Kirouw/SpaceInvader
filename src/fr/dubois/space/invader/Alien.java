01d7c77dee6f928999c718378103a16b5e14c7b9
selim.boughriet <selim.boughriet@103F15.etu.iut-lens.univ-artois.fr>
2012-11-26 12:26:44 +0100

    Modification de act()

--- src/fr/dubois/space/invader/Alien.java
+++ src/fr/dubois/space/invader/Alien.java
@@ -6,20 +6,15 @@ public class Alien extends Sprite{
 
 	public Alien(Bitmap bitmap, float x, float y) {
 		super(bitmap, x, y);
-		// TODO Auto-generated constructor stub
 	}
-
 	@Override
 	public void act() {
-		// TODO Auto-generated method stub
-		if(x<=600)
-		{
 			x = x+5;
-		}
-		else
-		{
-			x = x+0;
-		}
+			if(x >= 200)
+			{
+				x = x;
+				y = y-1;
+			}
 	}
 
 }
