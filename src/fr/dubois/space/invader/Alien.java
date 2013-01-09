7966b41d963131a358c9a188d4046c2b8982069f
selim.boughriet <selim.boughriet@103F15.etu.iut-lens.univ-artois.fr>
2013-01-09 13:58:25 +0100

    Alien direction mouvant

--- src/fr/dubois/space/invader/Alien.java
+++ src/fr/dubois/space/invader/Alien.java
@@ -3,18 +3,36 @@ package fr.dubois.space.invader;
 import android.graphics.Bitmap;
 
 public class Alien extends Sprite{
+	
+	boolean direction;
 
 	public Alien(Bitmap bitmap, float x, float y) {
 		super(bitmap, x, y);
 	}
 	@Override
 	public void act() {
-			x = x+5;
-			if(x >= 200)
+		if(direction){
+			if(x >= 550)
 			{
-				x = x;
-				y = y-1;
+				direction = false;
+				y += 20;
+			} 
+			else{
+				x += 10;
+			} 
+		}
+		else
+		{			
+			if(x <= 0)
+			{				
+				direction = true;				
+				y += 20;			
+			} 
+			else
+			{
+				x -= 10;
 			}
+		}
 	}
 
 }
